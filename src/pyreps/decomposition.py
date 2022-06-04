7204bb474c5d9707b10f79fa2162376e3ca38851
Rafael Villarroel <rvf0068@gmail.com>
2022-06-03 20:44:41 -0500

    Several improvements
    Decomposition now works for small groups. But for the regular
    representation of S_4 it seems to take long.

--- src/pyreps/decomposition.py
+++ src/pyreps/decomposition.py
@@ -1,24 +1,20 @@
 import sympy as sp
 
-from sympy import eye, sqrt, I, sympify, expand, Abs
-from sympy.matrices import zeros
-
 from pyreps.matrix import MatrixRepresentation
 
 
 def block(M):
-    """A function that return where end the blocks of a matrix.
+    """A function that returns the indices of the columns where the blocks of a
+    matrix end.
 
     Args:
-        M (Matrix): The matrix which will be find their blocks.
+        M (Matrix): A sympy (square) matrix
 
     Returns:
-        v (list): A list that indicated where end the blocks
-            of a matrix.
+        v (list): A list that shows where the blocks
+            of a matrix end.
 
     Examples:
-        To find the blocks of a matrix use ``block(M)``.
-
         >>> from sympy.matrices import Matrix
         >>> from pyreps.decomposition import block
         >>> M = Matrix([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
@@ -34,8 +30,9 @@ def block(M):
         c = 0
         for j in range(c1, n):
             if (M[i, j] != 0 or M[j, i] != 0):
-                if (Abs(i-j) > c):
-                    c = Abs(i-j)
+                if (sp.Abs(i-j) > c):
+                    c = sp.Abs(i-j)
+                    # c is (size of the first block) - 1
         if (c == 0):
             v.append(c1)
             c1 = c1+1
@@ -47,8 +44,8 @@ def block(M):
                 for j in range(c1, c1+c+1):
                     for k in range(c1+c+1, n):
                         if (M[j, k] != 0 or M[k, j] != 0):
-                            if (Abs(i-k) > c):
-                                c = Abs(i-k)
+                            if (sp.Abs(i-k) > c):
+                                c = sp.Abs(i-k)
             v.append(c1+c)
             c1 = c1+c+1
             i = c1
@@ -56,23 +53,23 @@ def block(M):
 
 
 def blockI(M, n, i):
-    """A function that given a matrix, put it since the entry (i,i) of a
-    identity matrix of degree n.
+    """A function that inserts the matrix `M`, starting at the entry `(i, i)` of a
+    identity matrix of degree `n`.
 
     Args:
-        M (Matrix): The matrix which will be put in the entry (i,i) of
+        M (Matrix): The matrix which will be put in the entry `(i, i)` of
             a identity matrix of degree n.
 
-        n (int): Determine the size of the identity matrix.
+        n (int): Size of the resulting matrix.
 
-        i (int): A integer that will indicated the entry (i,i) of the
+        i (int): A integer that will indicated the entry `(i, i)` of the
             identity matrix.
     Returns:
-        N (Matrix): The identity matrix with contains the matrix M
-            in the entry (i,i).
+        N (Matrix): The identity matrix which contains the matrix `M`
+            in the entry `(i, i)`.
 
     Raises:
-        IndexError: If the number of the columns or the raws plus i are bigger
+        IndexError: If the number of the columns or the rows plus i are bigger
             to n.
 
     Examples:
@@ -81,7 +78,7 @@ def blockI(M, n, i):
         >>> from sympy.matrices import Matrix
         >>> from pyreps.decomposition import blockI
         >>> M = Matrix([[1, 1, 1], [1, 1, 1]])
-        >>> blockI(M,4,0)
+        >>> blockI(M, 4, 0)
         Matrix([
         [1, 1, 1, 0],
         [1, 1, 1, 0],
@@ -90,7 +87,7 @@ def blockI(M, n, i):
     
     """
 #    a=M.shape[0]
-    N = eye(n)
+    N = sp.eye(n)
     for j in range(0, M.shape[0]):
         for k in range(0, M.shape[1]):
             N[j+i, k+i] = M[j, k]
@@ -108,17 +105,14 @@ def MTS(A):
             V*AV=I.
 
     Examples:
-        To create this matrix use ``MTS(A)``.
-
         >>> from sympy.matrices import Matrix
         >>> M = Matrix([[1, 0, 1], [2, -1, 3], [4, 3, 2]])
-        >>> N = M.H
-        >>> A = N*M
+        >>> A = M.H*M
 
         .. Note:: A is positive definite Hermitian matrix.
 
         >>> from pyreps.decomposition import MTS
-        >>> V = MTS(N*M)
+        >>> V = MTS(A)
         >>> V
         Matrix([
         [sqrt(21)/21, -sqrt(2310)/231, -12*sqrt(110)/11],
@@ -129,73 +123,52 @@ def MTS(A):
         [1, 0, 0],
         [0, 1, 0],
         [0, 0, 1]])
-   
+
     """
     A1 = A
     n = A.shape[0]
-    V = eye(n)
-    for i in range(0, n):
-        C = eye(n)
-        C[i, i] = 1/sqrt(A1[i, i])
+    V = sp.eye(n)
+    for i in range(n):
+        C = sp.eye(n)
+        C[i, i] = 1/sp.sqrt(A1[i, i])
         for j in range(i+1, n):
             C[i,  j] = -(1/A1[i, i])*A1[i, j]
         V = V*C
-        V.simplify()
         A1 = (C.H)*A1*C
-        A1.simplify()
     return V
 
 
-def unitary_representation(G, d):
+def unitary_representation(d):
     """Becomes a matrix representation in a unitary matrix representation.
 
     Args:
-        d (dict): A dict that must contains the map of a group into the group
-            of nonsingular linear transformations of some finite dimensional
-            vector space.
-        G (sympy.combinatorics.perm_groups.PermutationGroup): The group.
+        d (pyreps.matrix.MatrixRepresentation): A pyreps representation
 
     Returns:
         __main__.MatrixRepresentation: A unitary matrix representation.
 
     """
+    G = d.group
     n = d.degree
-    A = zeros(n, n)
-    for g in d.map:
+    A = sp.zeros(n, n)
+    for g in d._elements:
         J = (d.map[g].H)*d.map[g]
         J = sp.expand(J)
         A = J+A
-    A1 = A
-    V = eye(n)
-    for i in range(0, n):
-        C = eye(n)
-        C[i, i] = 1/sqrt(A1[i, i])
-        for j in range(i+1, n):
-            C[i, j] = -(1/A1[i, i])*A1[i, j]
-        V = V*C
-        V = sp.expand(V)
-        A1 = (C.H)*A1*C
-        A1 = sp.expand(A1)
     V = MTS(A)
-    M = {}
-    for g in list(G.generate()):
-        M[g] = sp.ImmutableMatrix((V.inv())*d.map[g]*V)
+    M = {g: sp.ImmutableMatrix((V.inv())*d.map[g]*V) for g in d._elements}
     return MatrixRepresentation(M, G, n)
 
 
-def is_irreducible(G, d):
+def is_irreducible(d):
     """Determines if a representation is irreducible.
 
     Args:
-        d (dict): A dict that must contains the map of a group into the group
-            of nonsingular linear transformations of some finite dimensional
-            vector space.
-
-        G (sympy.combinatorics.perm_groups.PermutationGroup): The group.
+        d (pyreps.matrix.MatrixRepresentation): A pyreps representation
 
     Returns:
-        True if the representation is irreducible,  a matrix non
-        escalar that reduce the matrix representation in otherwise.
+        True if the representation is irreducible,  otherwise, a non scalar
+    matrix that reduces the matrix representation.
 
     Examples:
         To see if a representation is irreducible use
@@ -217,10 +190,11 @@ def is_irreducible(G, d):
         # [  0,   0,   0,   0, 1/3,   0]])
     """
     n = d.degree
-    N = eye(n)
-    for r in range(0, n):
-        for s in range(0, n):
-            H = zeros(n)
+    N = sp.eye(n)
+    R = unitary_representation(d)
+    for r in range(n):
+        for s in range(n):
+            H = sp.zeros(n)
             if (n-1-r == n-1-s):
                 H[n-1-r, n-1-r] = 1
             else:
@@ -228,41 +202,37 @@ def is_irreducible(G, d):
                     H[n-1-r, n-1-s] = 1
                     H[n-1-s, n-1-r] = 1
                 else:
-                    H[n-1-r, n-1-s] = 1*I
-                    H[n-1-s, n-1-r] = -1*I
-            M = zeros(n, n)
-            R = unitary_representation(G, d)
-            for g in R.map:
+                    H[n-1-r, n-1-s] = 1*sp.I
+                    H[n-1-s, n-1-r] = -1*sp.I
+            M = sp.zeros(n)
+            for g in R._elements:
                 M = M+(R.map[g].H*H*R.map[g])
-            M = (sympify(1)/n)*M
-            M = expand(M)
+            M = (sp.sympify(1)/n)*M
+            M = sp.expand(M)
             if (M != M[0, 0]*N):
                 return M
     else:
         return True
 
 
-def reduce(G, d):
+def reduce(d):
     """Decompose a representation into irreducibles
 
     Args:
-        G (Group): The group.
-
         d (dict): The representation.
 
     Returns:
-        U (Matrix): A matrix wich descompose the representation.
+        U (Matrix): A matrix which decomposes the representation.
     """
-    M = is_irreducible(G, d)
+    G = d.group
     b = d.degree
-    if M:
-        return(eye(b))
+    M = is_irreducible(d)
+    if M is True:
+        return(sp.eye(b))
     else:
         (P,  J) = M.jordan_form()
-        P = expand(P)
-        w = []
-        for g in d.map:
-            w.append(block(P.inv()*d.map[g]*P))
+        P = sp.expand(P)
+        w = [block(P.inv()*d.map[g]*P) for g in d._elements]
         length = len(w[0])
         au = w[0]
         for g in w:
@@ -272,9 +242,8 @@ def reduce(G, d):
         e = 0
         U = P
         for a in au:
-            d1 = {}
-            for g in list(G.generate()):
-                d1[g] = sp.ImmutableMatrix((P.inv()*d.map[g]*P)[e:a+1, e:a+1])
-            U = U*blockI(reduce(G, MatrixRepresentation(d1, G, (a+1-e))), b, e)
+            d1 = {g: sp.ImmutableMatrix((P.inv()*d.map[g]*P)[e:a+1, e:a+1])
+                  for g in d._elements}
+            U = U*blockI(reduce(MatrixRepresentation(d1, G, (a+1-e))), b, e)
             e = a+1
-        return U
+        return sp.ImmutableMatrix(U)
